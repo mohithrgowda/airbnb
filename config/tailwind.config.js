1f7e09fccf0245277aa9d57ba9eb4076e2df6b10
Mohith S R <mohithrgowda@Mohiths-Air.fritz.box>
2025-08-02 12:53:32 +0200

    setup home/index and added navbar using tailwind

---
+++ config/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#FF5A5F',
+        dark: '#FF385C',
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+};
